17af5a2dc8660ea88185460f6873328a369a9db3
Gero <gersonhoa@gmail.com>
2022-02-24 10:00:26 -0300

    feat : implement ToogleTheme button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './context/ThemeContext';
+import ThemeToogle from './components/ThemeToogle';
 
 function App() {
   return (
     <div className="App">
     <ThemeContextProvider>
       <Navbar />
-      <BookList/>
+      <BookList />
+      <ThemeToogle />
     </ThemeContextProvider>
     </div>
   );
