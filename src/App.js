e39006cd6ad74ce8e6763df44fff1f5afce43611
Gero <gersonhoa@gmail.com>
2022-02-24 12:06:40 -0300

    feat : cria a base do app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
-import ThemeContextProvider from './context/ThemeContext';
-import ThemeToogle from './components/ThemeToogle';
 
 function App() {
   return (
     <div className="App">
-    <ThemeContextProvider>
       <Navbar />
       <BookList />
-      <ThemeToogle />
-    </ThemeContextProvider>
     </div>
   );
 }
