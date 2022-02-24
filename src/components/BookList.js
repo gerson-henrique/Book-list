3d6cefa507b91d10aa03c4b783d740c0996e68c6
Gero <gersonhoa@gmail.com>
2022-02-24 12:08:59 -0300

    fix : arruma os envios do git

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+        <li>p1</li>
+        <li>p2</li>
+        <li>p3</li>
+        </ul>
+      </div>
+    )
+  }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1> Context App </h1>
+        <ul>
+          <li>Projeto</li>
+          <li>Jogar</li>
+          <li>Sobre Mim</li>
+        </ul>
+      </nav>
+    )
+  }
+}
