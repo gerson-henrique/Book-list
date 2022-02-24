7567c0256cfb88251cf29040584a381b8128b608
Gero <gersonhoa@gmail.com>
2022-02-24 15:15:06 -0300

    feat : cria o botão de login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import { AuthContext } from '../context/AuthContext';
 import { ThemeContext } from '../context/ThemeContext'
 
 export default class Navbar extends Component {
   render() {
     return (
+      <AuthContext.Consumer>{(authContext) => (
       <ThemeContext.Consumer>{(themeContext) =>{
       
+      const {isAuthenticated, toogleAuth} = authContext;
       const {isLightMode, light, dark} = themeContext;
       const theme = isLightMode ? light : dark;
       
       return (<nav style={{background:theme.ui, color:theme.syntax}}>
        <h1> Context App</h1>
+       <div onClick={toogleAuth}>
+         {isAuthenticated ? 'Loged in' : 'Loged out'}
+       </div>
         <ul>
         <li>Home</li>
         <li>About</li>
@@ -18,7 +24,8 @@ export default class Navbar extends Component {
         </ul>
       </nav>)
 }}
-      </ThemeContext.Consumer>
+      </ThemeContext.Consumer>)}
+      </AuthContext.Consumer>
     )
   }
 }
